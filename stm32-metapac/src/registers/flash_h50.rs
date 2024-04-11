aba9d07d132e7a3ed0b83f29fa767fdf13d091a5
Warren Campbell <wcampbell@ara.com>
2024-04-11 18:17:20 -0400

    Corrects STM32U5 memory sizes

--- stm32-metapac/src/registers/flash_h50.rs
+++ stm32-metapac/src/registers/flash_h50.rs
@@ -1,6 +1,6 @@
 
-use crate::metadata::ir::*;
-pub(crate) static REGISTERS: IR = IR {
+                    use crate::metadata::ir::*;
+                    pub(crate) static REGISTERS: IR = IR {
     blocks: &[
         Block {
             name: "Flash",
@@ -2509,3 +2509,4 @@ pub(crate) static REGISTERS: IR = IR {
         },
     ],
 };
+                
